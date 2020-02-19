752d8693fc109b4dbcd2c534e2b8e6feae3d3dc0
Paulo Ricardo Silva de Araújo <54222357+pkomander@users.noreply.github.com>
2020-02-19 01:35:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Recepção Humana - Projeto Integrador">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+  <meta name="author" content="Guilherme Santos, Paulo Ricardo">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Recepção Humana - Projeto Integrador</title>
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+
+</head>
+<body>
+
+  	<div class="container">
+  		<div class="row topo">
+
+  			<!--  Titulo da Página   -->
+	     	<div class="col-12 topo_nome_proj">
+		      	RECEPÇÃO HUMANA - CURSO
+		    </div>
+
+		    <!--  Incluindo a pagina menu.php dentro dessa pagina   -->
+		    <div class="menu_nav">
+      			<?php require_once('menu.php'); ?>
+  			</div>
+  		</div>
+
+  		<!--  Conteudo   -->
+  		<div class="row col-12 conteudo">
+  				<h2>JUSTIFICATIVA </h2>
+  				Ultimamente o mercado de trabalho está cada vez mais competitivo, exigindo diversas habilidades técnicas dos profissionais, tornando o atendimento muitas
+				vezes robotizado. Percebe-se um certo despreparo emocional, principalmente
+				do profissional de atendimento ao público, em lidar com determinadas
+				situações, além do pouco tempo disponibilizado para treinamento de equipes.
+				Diante disso, surge a necessidade de auxiliar o treinamento e desenvolvimento
+				destes profissionais, trazendo a tecnologia como aliada através de um
+				aplicativo para auxiliar no desenvolvimento profissional de maneira ágil, pois
+				boa parte da população tem acesso fácil a aplicativos e dessa forma poderá ter
+				uma maior abrangência a estes profissionais.
+  		</div>
+
+
+
+  	</div>
+
+
+
+  
+
+
+
+	
+	
+
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
